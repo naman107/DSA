31be6b8edd8ee9dc979284e9cfc18e7202567efd
Naman Agarwal <naman.agarwal@bolt.earth>
2022-05-17 01:15:52 +0530

    Link:: https://codeforces.com/problemset/problem/71/A

---
+++ src/codeforce/LongWords.java
@@ -0,0 +1,28 @@
+package codeforce;
+
+import java.util.Scanner;
+
+public class LongWords {
+	
+	static Scanner sr = new Scanner(System.in);
+	public static void main(String args[])
+	{
+		double length = sr.nextInt();
+		String in = null;
+		
+		for(int i = 0; i <= length; i++)
+		{
+			in = sr.nextLine();
+			
+			if(in.length() > 10)
+			{
+				System.out.print(in.charAt(0));
+				System.out.print(in.length() -2); 
+				System.out.println(in.charAt(in.length() -1));
+			}else
+				System.out.println(in);
+			
+		}
+	}
+	
+}
